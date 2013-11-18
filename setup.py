33dc3d272f97532b3ad8d15b6ae5cd2e0278e835
Vickleford <vic.watkins@rackspace.com>
2013-11-18 10:33:42 -0600

    fixed naming conflict with similar projects

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 
 setup(name='agent.http',
-      version='0.3',
+      version='0.3.1',
       description='Rackspace Monitoring agent.plugin HTTP check',
       author='Vic Watkins',
       author_email='vic.watkins@rackspace.com',
       url='https://github.com/vickleford/agent.http',
       install_requires=['requests', 'argparse', 'logging'],
       packages=find_packages(),
-      py_modules=['check'],
+      py_modules=['agent_http'],
       entry_points = { 'console_scripts': [
-        'agent.http = check:spawn'
+        'agent.http = agent_http:spawn'
       ] }
      )
